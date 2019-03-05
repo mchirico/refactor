3b0deabf8c797ed9ed8eeb42cde2002b7fdce053
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-03-04 19:43:41 -0500

    passing

---
+++ test/billing.spec.ts
@@ -0,0 +1,30 @@
+var chai = require('chai')
+    , spies = require('chai-spies');
+chai.use(spies);
+chai.use(require('chai-string'));
+
+var expect = chai.expect;
+
+
+
+let billing = require("../app/billing");
+let plays = require('../app/plays.json');
+let invoices = require('../app/invoices.json');
+
+
+describe("Testing Billing", function () {
+    describe("Chapter 1 program", function () {
+        it("calculates the bill", function () {
+
+            let result = billing.statement(invoices[0], plays);
+
+            console.log(result)
+
+            expect(result).to.containIgnoreSpaces("Hamlet: $650.00 (55 seats)")
+            expect(result).to.containIgnoreSpaces("As You Like It: $580.00 (35 seats)")
+            expect(result).to.containIgnoreSpaces("Othello: $500.00 (40 seats)")
+            expect(result).to.containIgnoreSpaces("owed is $1,730.00")
+            expect(result).to.containIgnoreSpaces("earned 47 credits")
+        });
+    });
+});
