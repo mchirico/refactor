112f50a78dcd2d031e9ae0a8af8568c7ee06051f
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-03-05 15:51:27 -0500

    update

--- technique/technique.ts
+++ technique/technique.ts
@@ -11,14 +11,14 @@ class Greeter {
 
 exports.squareOf = function squareOf(n) {
     return n * n;
-}
+};
 
 exports.loopWPipe = function loopWPipe(names) {
     const result = [];
     for (const i of names) {
-        if (i.name === "Abby")
+        if (i.name === "Abby") {
             result.push(i.job);
+           }
     }
-
     return result;
 }
